d6db11c0246989d79724075e2ecd13ae54cdaead
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-01-19 13:32:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,153 @@
+// console.log("Hello People");
+// alert("hello");
+
+
+// create a javascript file, embedd the javascript file inside your html file and show an alert box saying, helloo everyone, welcome to my website
+
+
+
+
+
+
+
+
+
+
+
+
+
+//redeclaring
+// var a=3;//104 line
+
+// var a=4; // 1024 line
+
+// 3000 lines of code
+
+
+//ES6 or modern javascript
+// let a=2;
+// a=3;//reassigning variables
+// const b=2;
+// b=5;//cannot be reassigned
+// console.log(b);
+
+
+
+// declare 3 varibales
+
+// a=>using var 
+// b=>using let
+// c=>using const 
+
+// redeclare every varibale 
+
+
+// reassign every varibale 
+
+// and at last print every varibales 
+
+
+
+
+
+//set of instructions
+//reusable piece of code using arguments
+//we have to call this piece of code for it's execution
+
+//param 1 and param 2 are called as arguments/parameters
+
+
+// hello("hemlo","everyone")
+
+
+// param=>"hemlo"
+// param2=>"everyone"
+
+
+// hello("gg","wp")
+
+
+
+
+
+
+
+// function hello(param="asd",param2="awsdsdxcv"){
+//  console.log(param,param2);
+// }
+
+// hello("Hemlo","everyone")
+// hello("gg","wp")
+
+
+// create a reusable function, name it anything you want and log the  dynamic output in the console
+
+
+// hello("tyest","pppaaa");
+
+
+
+// function hello() {
+//     console.log("hemlo");
+// }
+
+// const hello = (arg1="asd",arg2="aaa")=>{
+//     console.log(arg1,arg2);
+// } 
+
+// hello()
+
+
+
+// declare an arrow functions with arguments/parameters and log the parameters/arguments in the console.also if no argument is given to the function, there should be a default value printed and not undefined
+
+
+// const arrFun = (param="gg",param2="wp")=>{
+//     console.log(param,param2);
+// }
+
+// arrFun();
+
+
+
+// make a function to add 2 numbers
+
+// do operations like 2+3
+
+// do operations like 5+6
+
+// do operations like 11+3
+
+// log the output in teh console
+
+
+
+// function add(a,c,...b) {
+//     console.log(a,c,b);
+// }
+// a=>2
+// b=>[3,45,6,7,8,8]
+// add(2,3,45,6,7,8,8)
+// add(2,3,6) gives NaN=>not a number because 2+3+6+undefined+undefined+undefined=Nan
+
+
+
+// let a = "Hey"
+
+// let b = "everyone,"
+
+// let c = "how are you"
+
+// let d = "All gucci"
+
+// let e ="yuss"
+
+
+// console.log(a+" "+b+" "+c+"? "+d+" => "+e);
+
+// console.log(`${a} ${b} ${c}? ${d} => ${e}`);
+
+
+
+
+// create 7 different varibales and combine them to make a single output that should be shown on the console
