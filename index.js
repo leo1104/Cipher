329bcf7b74d8a906023b1387faaeaaf5b7065f26
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-01-23 13:16:20 +0530

    22/01

--- index.js
+++ index.js
@@ -218,3 +218,125 @@
 
 
 // Also, print name, rollNumber and subject array  value in seperate console
+
+// 5 minutes 
+//try mapping array of objects 
+
+
+// const a=[{name:'asd'},{name:'test'},{name:'test1'}]
+
+
+// a.map((ele)=>{
+//     console.log(ele.name);
+// })
+
+
+
+//   given the data, try to map username and message available in the data, and log each of them in the console like
+
+//   => "username message"
+
+
+// a.map((ele)=>{
+//     console.log(`${ele.message} ${ele.notificationTriggeredUser.username}`);
+// })
+
+// const a =[1,2,3,4,5,6];
+
+
+// want to print those numbers which are greater than or equal to 4
+
+
+
+// const b = a.filter((ele)=>{
+// return ele>=4
+// });
+
+// console.log(b);
+
+
+//   create an array which contains notificationtype property as newPostUpVote using filter method 
+
+
+// const b= a.filter((ele)=>{
+//     return ele.notificationtype==='newPostUpVote'
+// })
+
+// console.log(b);
+
+let a =[1,2,3,4,5,6];
+
+let b= a.reduce((h1,h2)=>{
+    console.log(h1,h2);
+    return h1+h2
+})
+
+console.log(b);
+
+//there is an array => given 
+//sum of all elements
+
+
+// 1st=>
+
+// h1=>1
+// h2=>2
+
+// 2nd
+
+// h1=> 3
+// h2=>3
+
+// 3rd 
+
+// h1=>6
+// h2=>4
+
+// 4th
+
+// h1=>10
+// h2=>5
+
+// 5th
+// h1=>15
+// h2=>6
+
+// 6th
+
+// h1=> 21
+
+
+
+
+
+// create 3 Html elements with the same function  onclick 
+//create a function with arguments/parameters and pass this function to your element with different arguments and get the result and print it in console with arguments/parameters
+
+
+
+// let a = document.getElementById('test');
+
+// let b = document.getElementsByClassName('paraTwo')
+
+// // console.log(b);
+
+// function myFun(param1) {
+//     console.log(param1);
+// }
+
+//create 3 elements shoudl be different ex:a,div,p,h1 with same classname and in the console just log the 1st element of the same classname
+
+// a.addEventListener('click',function(){
+//     myFun('hello')
+// })
+
+//create an element with an classname and add an event listerner "click" and execute a function which logs something in the console//
+
+// note:don't use onclick in html
+//note: there should be 2 elemnts with the same classname 
+
+
+// create 2 elements with same classname and excute a function which takes 2 numbers in parameters/arguments and return the sum in console .
+
+// Note: the function should execute on click 
+// Note: don't use HTML onclick
